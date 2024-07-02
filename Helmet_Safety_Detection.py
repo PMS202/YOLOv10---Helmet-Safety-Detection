8a0d485383b602df75b1e03534e3f142f9de576b
PMS202 <misalol2510@gmail.com>
2024-07-02 22:16:04 +0700

    Nop project YOLOv10

--- Helmet_Safety_Detection.py
+++ Helmet_Safety_Detection.py
@@ -6,17 +6,18 @@ from ultralytics import YOLOv10
 MODEL = r"D:\Hoc\AIO2024\YOLOv10---Helmet-Safety-Detection\yolov10\runs\detect\train\weights\best.pt"
 
 
-def process_image(image):
-    image = cv2.resize()
+def predict(image):
     model = YOLOv10(MODEL)
     CONF_THRESHOLD = 0.3
+    IMAGE_URL = image
+    IMG_SIZE = 640
     results = model.predict(source=IMAGE_URL,
                             imgsz=IMG_SIZE,
                             conf=CONF_THRESHOLD)
     return results
 
 
-def main():
+def streamlit():
     """Main function for the image object detection app."""
 
     st.title("Object Detection for Images")
@@ -26,16 +27,14 @@ def main():
 
     if uploaded_file is not None:
         st.image(uploaded_file, caption="Uploaded Image")
-
-        image = Image.open(uploaded_file)
-        image = np.array(image)
-
+        image = Image.open(uploaded_file,)
         # Assuming process_image and annotate_image functions exist
-        detections = process_image(image)
-        processed_image = annotate_image(image, detections)
+        detections = predict(image)
+        annotated_img = detections[0].plot()
+        annotated_img = cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB)
 
-        st.image(processed_image, caption="Processed Image")
+        st.image(annotated_img, caption="Processed Image")
 
 
 if __name__ == "__main__":
-    main()
+    streamlit()
